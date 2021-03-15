0dd9a6eae9b5cc6d76646c6a708a17046e874ab0
Teslim Olunlade <to44@njit.edu>
2021-03-15 20:40:55 +0000

    Finishing touches

--- test/unmocked/test_move.py
+++ test/unmocked/test_move.py
@@ -10,7 +10,7 @@ from app import on_move
 class TestOnMove(unittest.TestCase):
     def test_move_success(self):
         self.assertEqual(on_move(0, [1, 2, 3, 4, 5, 6, 7, 8]), "Tie")
-        self.assertEqual(on_move(7, [0, 2, 5, 3, 1, 4, 6]), None)
+        self.assertEqual(on_move(7, [0, 2, 5, 3, 1, 4, 6]), "Continue")
 
 
 if __name__ == "__main__":
