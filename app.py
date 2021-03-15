2b241cfeb7923458d0dd9cf8832092fbd93967bb
Teslim Olunlade <to44@njit.edu>
2021-03-15 00:19:48 +0000

    Fix for app context with database

--- app.py
+++ app.py
@@ -22,7 +22,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
-db.create_all()
+
+with app.app_context():
+    db.create_all()
 
 socketio = SocketIO(app, cors_allowed_origins="*", json=json, manage_session=False)
 
